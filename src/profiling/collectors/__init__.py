86ec3041ec09ac841b3c68fffc910e8704a484bb
Egor Plehanov <egor.plekhanov00@mail.ru>
2026-02-04 17:40:36 +0300

    prof coll

---
+++ src/profiling/collectors/__init__.py
@@ -0,0 +1,12 @@
+# src/profiling/collectors/__init__.py
+"""
+Пакет коллекторов для профилирования.
+"""
+
+from .system_collector import SystemCollector, create_system_collector, system_collector
+
+__all__ = [
+    'SystemCollector',
+    'create_system_collector',
+    'system_collector'
+]
