ca3edfef63c1bfdf1949a77f4543f64b6831c54d
Egor Plehanov <58941865+EgorPlehanov@users.noreply.github.com>
2026-02-09 16:44:35 +0300

    Pass scalene filters before script path

--- src/profiling/collectors/scalene_collector.py
+++ src/profiling/collectors/scalene_collector.py
@@ -72,13 +72,13 @@ class ScaleneCollector:
             "--no-browser",
             "--outfile",
             str(html_path),
-            str(script_path),
         ]
         profile_just = self._build_profile_just_regex()
         if profile_just:
             args.extend(["--profile-just", profile_just])
         if script_args:
             args.extend(script_args)
+        args.append(str(script_path))
 
         try:
             subprocess.run(args, check=True, capture_output=True, text=True)
